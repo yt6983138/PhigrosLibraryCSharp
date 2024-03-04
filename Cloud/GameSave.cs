554b8cac94cd6bd5b44d27b13897fe1c1025aa7b
yt6983138 <yt6983138@gmail.com>
2024-03-04 17:30:22 +0800

    Add project files.

---
+++ Cloud/GameSave.cs
@@ -0,0 +1,9 @@
+﻿namespace PhigrosLibraryCSharp;
+
+public class GameSave
+{
+	public required List<InternalScoreFormat> Records { get; set; }
+	public required DateTime CreationDate { get; set; }
+	public required DateTime ModificationTime { get; set; }
+	public string Summary { get; set; } = ""; // unused, unknown	
+}
---
+++ Cloud/UserInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace PhigrosLibraryCSharp;
+
+public struct UserInfo
+{
+	public string NickName { get; set; }
+	public string UserName { get; set; }
+	public DateTime CreationTime { get; set; }
+	public DateTime ModificationTime { get; set; }
+}
