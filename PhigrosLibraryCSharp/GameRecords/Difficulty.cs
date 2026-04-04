1ca246c1cf0d0643730c11ad4f2ce5775837ae52
yt6983138 <yt6983138@gmail.com>
2026-04-04 15:46:37 +0800

    Add Legacy and SP into Difficulty enum, for serialization usages

--- PhigrosLibraryCSharp/GameRecords/Difficulty.cs
+++ PhigrosLibraryCSharp/GameRecords/Difficulty.cs
@@ -21,4 +21,12 @@ public enum Difficulty
 	/// AT, another
 	/// </summary>
 	AT = 3,
+	/// <summary>
+	/// Legacy, only for some old charts, not used anymore, kept for serialization
+	/// </summary>
+	Legacy = 4,
+	/// <summary>
+	/// Special, only for special charts such as April fools chart, kept for serialization
+	/// </summary>
+	SP = 5
 }
